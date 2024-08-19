6f65e2e53c914dc2b64ff85768e2b6cdb3a69ed4
Wolf <124432831+MasTerOkk@users.noreply.github.com>
2024-08-19 18:42:10 +0300

    Token expiration is 24 hour. Forbidding on registration of already used mail

--- src/main/java/ru/capybara/pizza/service/AuthenticationService.java
+++ src/main/java/ru/capybara/pizza/service/AuthenticationService.java
@@ -39,6 +39,9 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse register(RegisterRequest request) {
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException("This email address is busy");
+        }
         User user = User.builder()
                 .firstname(request.getFirstname())
                 .lastname(request.getLastname())
--- src/main/java/ru/capybara/pizza/service/impl/JwtServiceImpl.java
+++ src/main/java/ru/capybara/pizza/service/impl/JwtServiceImpl.java
@@ -41,7 +41,7 @@ public class JwtServiceImpl implements JwtService {
                 .claims(extractClaims)
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
